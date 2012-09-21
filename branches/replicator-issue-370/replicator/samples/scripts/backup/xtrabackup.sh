a8fa1b6c41aa4a53725cfeef7e960c0025f7193d
robert.hodges@continuent.com <robert.hodges@continuent.com@9f5e3340-5c9a-25cb-7c1c-c133a2e9e928>
2012-09-21 20:28:35 +0000

    Issue 370 Setting up independent branch for refactoring of CVS loading.

---
+++ branches/replicator-issue-370/replicator/samples/scripts/backup/xtrabackup.sh
@@ -0,0 +1,157 @@
+#!/bin/bash -eu
+# -e: Exit immediately if a command exits with a non-zero status.
+# -u: Treat unset variables as an error when substituting.
+# 
+# Custom backup script that follows conventions for script backups implemented
+# by ScriptBackupAgent.  
+#
+
+usage() {
+  echo "Usage: $0 {-backup|-restore} -properties file [-options opts]"
+}
+
+# Parse arguments. 
+operation=
+options=
+properties=
+
+user=root
+password=
+host=localhost
+port=3306
+directory=/tmp/innobackup
+archive=/tmp/innobackup.tar.gz
+mysql_service_command="/etc/init.d/mysql"
+mysqldatadir=/var/lib/mysql
+mysqllogdir=/var/lib/mysql
+mysqllogpattern=mysql-bin
+mysqluser=mysql
+mysqlgroup=mysql
+
+while [ $# -gt 0 ]
+do
+  case "$1" in
+    -backup) 
+      operation="backup";;
+    -restore) 
+      operation="restore";;
+    -properties)
+      properties="$2"; shift;;
+    -options)
+      options="$2"; shift;;
+    *)  
+      echo "unrecognized option: $1"
+      usage;
+      exit 1;
+  esac
+  shift
+done
+
+#
+# Break apart the options and assign them to variables
+#
+for i in `echo $options | tr '&' '\n'`
+do
+  parts=(`echo $i | tr '=' '\n'`)
+  eval $parts=${parts[1]}
+done
+
+# Note 1.
+# Do not use 'which' to determine where the 'service' command is.
+# The command will fail if 'service' is not in the PATH, and the whole script will fail.
+# Moreover, a simple 'sudo' will not get '/sbin' in $PATH, and the comamnd is likely to fail.
+#
+# Note 2.
+# Use '-x' to check for executables. Using '-f' will fail if the file is there but it is 
+# not executable.
+if [ ! -x $mysql_service_command ]
+then
+  service_command=/sbin/service
+  if [ -x $service_command ]
+  then
+      mysql_service_command="$service_command mysql"
+  elif [ -x "/etc/init.d/mysqld" ]
+  then
+      mysql_service_command="/etc/init.d/mysqld"
+  elif [ -x "/etc/init.d/mysql" ]
+  then
+      mysql_service_command="$/etc/init.d/mysql"
+  else
+      echo "Unable to determine the service command to start/stop mysql" >&2
+      exit 1
+  fi
+fi
+
+# Handle operation. 
+if [ "$operation" = "backup" ]; then
+  # Echo backup file to properties. 
+  echo "file=$archive" > $properties
+
+  # Clean up the filesystem before starting
+  rm -rf $directory
+  rm -f $archive
+
+  # Copy the database files and apply any pending log entries
+  innobackupex-1.5.1 --user=$user --password=$password --host=$host --port=$port --no-timestamp $directory
+  innobackupex-1.5.1 --apply-log --user=$user --password=$password --host=$host --port=$port $directory
+
+  # Package up the files and remove the staging directory
+  cd $directory
+  tar -czf $archive *
+  rm -rf $directory
+
+  exit 0
+elif [ "$operation" = "restore" ]; then
+  if [ ! -d "$mysqllogdir" ]; then
+    echo "The MySQL log dir '$mysqllogdir' is not a directory" >&2
+    exit 1
+  fi
+  
+  if [ ! -d "$mysqldatadir" ]; then
+    echo "The MySQL data dir '$mysqldatadir' is not a directory" >&2
+    exit 1
+  fi
+  
+  # Get the name of the backup file and restore.  
+  . $properties
+
+  # Clean up the filesystem before starting
+  rm -rf $directory
+  mkdir $directory
+  cd $directory
+  
+  # Unpack the backup files
+  tar -xzf $file
+
+  # Stop mysql and clear the mysql data directory
+  $mysql_service_command stop 1>&2
+
+  # We are expecting the exit code to be 3 so we have to turn off the 
+  # error trapping
+  set +e
+  `mysql -u$user -p$password -h$host -P$port -e "select 1"` > /dev/null 2>&1
+  if [ $? -ne 1 ]; then
+    echo "Unable to properly shutdown the MySQL service" >&2
+    exit 1
+  fi
+  set -e
+  
+  rm -rf $mysqldatadir/*
+  rm -rf $mysqllogdir/$mysqllogpattern.*
+
+  # Copy the backup files to the mysql data directory
+  innobackupex-1.5.1 --ibbackup=xtrabackup_51 --copy-back $directory
+
+  # Fix the permissions and restart the service
+  chown -RL $mysqluser:$mysqlgroup $mysqldatadir
+  $mysql_service_command start 1>&2
+
+  # Remove the staging directory
+  rm -rf $directory
+  
+  exit 0
+else
+  echo "Must specify -backup or -restore"
+  usage
+  exit 1
+fi
---
+++ branches/replicator-issue-370/replicator/src/java/com/continuent/tungsten/replicator/thl/RemoteTHLExtractor.java
@@ -0,0 +1,471 @@
+/**
+ * Tungsten Scale-Out Stack
+ * Copyright (C) 2010-2011 Continuent Inc.
+ * Contact: tungsten@continuent.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ *
+ * Initial developer(s): Robert Hodges
+ * Contributor(s):
+ */
+
+package com.continuent.tungsten.replicator.thl;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.continuent.tungsten.commons.cluster.resource.OpenReplicatorParams;
+import com.continuent.tungsten.replicator.InSequenceNotification;
+import com.continuent.tungsten.replicator.OutOfSequenceNotification;
+import com.continuent.tungsten.replicator.ReplicatorException;
+import com.continuent.tungsten.replicator.conf.ReplicatorConf;
+import com.continuent.tungsten.replicator.event.ReplDBMSEvent;
+import com.continuent.tungsten.replicator.event.ReplDBMSFilteredEvent;
+import com.continuent.tungsten.replicator.event.ReplDBMSHeader;
+import com.continuent.tungsten.replicator.event.ReplEvent;
+import com.continuent.tungsten.replicator.extractor.Extractor;
+import com.continuent.tungsten.replicator.extractor.ExtractorException;
+import com.continuent.tungsten.replicator.plugin.PluginContext;
+import com.continuent.tungsten.replicator.plugin.PluginLoader;
+import com.continuent.tungsten.replicator.plugin.ShutdownHook;
+
+/**
+ * Implements an extractor to pull events from a remote THL.
+ * <p/>
+ * This class has specialized concurrency requirements as there is a potential
+ * race condition to close connections within the task thread and thread trying
+ * to shut down the pipeline. The race arises due the fact that connections may
+ * hang when connecting or reading from a connection to a dropped interface and
+ * do not accept interrupts. They need to be interrupted by closing the
+ * connection. For this reason, closing the connection is synchronized.
+ * 
+ * @author <a href="mailto:robert.hodges@continuent.com">Robert Hodges</a>
+ * @version 1.0
+ */
+public class RemoteTHLExtractor implements Extractor, ShutdownHook
+{
+    private static Logger    logger             = Logger.getLogger(RemoteTHLExtractor.class);
+
+    // Properties.
+    private List<String>     uriList;
+    private int              resetPeriod        = 1;
+    private boolean          checkSerialization = true;
+    private int              heartbeatMillis    = 3000;
+
+    // Connection control variables.
+    private PluginContext    pluginContext;
+    private ReplDBMSHeader   lastEvent;
+    private String           lastEventId;
+    private Connector        conn;
+
+    private ReplEvent        pendingEvent;
+
+    // Set to show that we have been shut down.
+    private volatile boolean shutdown           = false;
+
+    /**
+     * Create Connector instance.
+     */
+    public RemoteTHLExtractor()
+    {
+    }
+
+    public List<String> getConnectUri()
+    {
+        return uriList;
+    }
+
+    /**
+     * Set the URI(s) of the store to which we connect.
+     * 
+     * @param connectUri
+     */
+    public void setConnectUri(List<String> connectUri)
+    {
+        this.uriList = connectUri;
+    }
+
+    public int getResetPeriod()
+    {
+        return resetPeriod;
+    }
+
+    /**
+     * Set the number of iterations before resetting the communications stream.
+     * Higher values use more memory but are more efficient.
+     */
+    public void setResetPeriod(int resetPeriod)
+    {
+        this.resetPeriod = resetPeriod;
+    }
+
+    public boolean isCheckSerialization()
+    {
+        return checkSerialization;
+    }
+
+    /**
+     * If true, check epoch number and sequence number of last event we have
+     * received.
+     * 
+     * @param checkSerialization
+     */
+    public void setCheckSerialization(boolean checkSerialization)
+    {
+        this.checkSerialization = checkSerialization;
+    }
+
+    public int getHeartbeatInterval()
+    {
+        return heartbeatMillis;
+    }
+
+    /**
+     * Sets the interval for sending heartbeat events from server to avoid
+     * TCP/IP timeout on server connection.
+     */
+    public void setHeartbeatInterval(int heartbeatMillis)
+    {
+        this.heartbeatMillis = heartbeatMillis;
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see com.continuent.tungsten.replicator.extractor.Extractor#extract()
+     */
+    public ReplDBMSEvent extract() throws ReplicatorException,
+            InterruptedException
+    {
+        try
+        {
+            // Open the connector if it is not yet open.
+            if (conn == null)
+            {
+                openConnector();
+            }
+
+            // Fetch the event.
+            ReplEvent replEvent = null;
+            while (replEvent == null && !shutdown)
+            {
+                // If we have a pending event from an earlier read, return that.
+                if (pendingEvent != null)
+                {
+                    replEvent = pendingEvent;
+                    pendingEvent = null;
+                    break;
+                }
+
+                long seqno = 0;
+                try
+                {
+                    if (lastEvent != null)
+                        if (lastEvent.getLastFrag())
+                        {
+                            if (lastEvent instanceof ReplDBMSFilteredEvent)
+                            {
+                                ReplDBMSFilteredEvent ev = (ReplDBMSFilteredEvent) lastEvent;
+                                seqno = ev.getSeqnoEnd() + 1;
+                            }
+                            else
+                                seqno = lastEvent.getSeqno() + 1;
+                        }
+                        else
+                            seqno = lastEvent.getSeqno();
+                    replEvent = conn.requestEvent(seqno);
+                    if (replEvent == null)
+                        continue;
+
+                    // If the lastEventId was set, we have some housekeeping
+                    // ahead of us.
+                    if (lastEventId != null)
+                    {
+                        // Searching for lastEventId can cause skips in the
+                        // log. If so, we need to insert a filter event to
+                        // avoid breaks and return that first. Otherwise
+                        // downstream stages will break due to sequence number
+                        // gaps.
+                        if (lastEvent != null && replEvent.getSeqno() > seqno)
+                        {
+                            pendingEvent = replEvent;
+                            replEvent = new ReplDBMSFilteredEvent(lastEventId,
+                                    seqno, replEvent.getSeqno() - 1, (short) 0);
+                        }
+
+                        // Next, clear the last event ID.
+                        if (logger.isDebugEnabled())
+                        {
+                            logger.debug("Clearing last event ID: "
+                                    + lastEventId);
+                        }
+                        lastEventId = null;
+                    }
+                }
+                catch (IOException e)
+                {
+                    if (shutdown)
+                    {
+                        if (logger.isDebugEnabled())
+                        {
+                            logger.debug(
+                                    "Ignoring exception after shutdown request",
+                                    e);
+                        }
+                        logger.info("Connector read failed after shutdown; not attempting to reconnect");
+                        break;
+                    }
+                    else
+                    {
+                        // If the connection dropped in the middle of a
+                        // fragmented transaction, we need to ignore events that
+                        // were already stored, otherwise it will generate an
+                        // integrity constraint violation
+                        reconnect();
+                        continue;
+                    }
+                }
+
+                // Ensure we have the right *sort* of replication event.
+                if (replEvent != null && !(replEvent instanceof ReplDBMSEvent))
+                    throw new ExtractorException(
+                            "Unexpected event type: seqno =" + seqno + " type="
+                                    + replEvent.getClass().getName());
+            }
+
+            // Remember which event we just read and ask for the next one.
+            lastEvent = (ReplDBMSEvent) replEvent;
+            return (ReplDBMSEvent) replEvent;
+        }
+        catch (THLException e)
+        {
+            // THLException messages are user-readable so just pass 'em along.
+            throw new ExtractorException(e.getMessage(), e);
+        }
+
+    }
+
+    /** Does not make sense for this extractor type. */
+    public String getCurrentResourceEventId() throws ReplicatorException,
+            InterruptedException
+    {
+        return null;
+    }
+
+    public boolean hasMoreEvents()
+    {
+        return false;
+    }
+
+    public void setLastEvent(ReplDBMSHeader event) throws ReplicatorException
+    {
+        lastEvent = event;
+    }
+
+    /**
+     * Sets the last event ID for extraction. If this is set, we will request
+     * (and receive) the first event from the master log that matches this
+     * event.
+     * 
+     * @see com.continuent.tungsten.replicator.extractor.Extractor#setLastEventId(java.lang.String)
+     */
+    public void setLastEventId(String eventId) throws ReplicatorException
+    {
+        if (logger.isDebugEnabled())
+            logger.debug("Set last event ID on remote THL extractor: "
+                    + eventId);
+        lastEventId = eventId;
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see com.continuent.tungsten.replicator.plugin.ReplicatorPlugin#configure(com.continuent.tungsten.replicator.plugin.PluginContext)
+     */
+    public void configure(PluginContext context) throws ReplicatorException
+    {
+        // Store context for later.
+        this.pluginContext = context;
+
+        // Set the connect URI to a default if not already set.
+        if (this.uriList == null || this.uriList.size() == 0)
+        {
+            uriList = new ArrayList<String>();
+            uriList.add(context.getReplicatorProperties().get(
+                    ReplicatorConf.MASTER_CONNECT_URI));
+        }
+
+        // See if we have an online option that overrides serialization
+        // checking.
+        if (pluginContext.getOnlineOptions().getBoolean(
+                OpenReplicatorParams.FORCE))
+        {
+            if (checkSerialization)
+            {
+                logger.info("Force option enabled; log serialization checking is disabled");
+                checkSerialization = false;
+            }
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see com.continuent.tungsten.replicator.plugin.ReplicatorPlugin#prepare(com.continuent.tungsten.replicator.plugin.PluginContext)
+     */
+    public void prepare(PluginContext context) throws ReplicatorException
+    {
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see com.continuent.tungsten.replicator.plugin.ReplicatorPlugin#release(com.continuent.tungsten.replicator.plugin.PluginContext)
+     */
+    public void release(PluginContext context) throws ReplicatorException
+    {
+        // Clearing the connection must be synchronized.
+        // See concurrency note in class header comment.
+        synchronized (this)
+        {
+            if (conn != null)
+            {
+                conn.close();
+                // Do not clear variable. It can cause an NPR in the
+                // openConnector() method which may still be attempting to
+                // open a connection.
+            }
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see com.continuent.tungsten.replicator.plugin.ShutdownHook#shutdown(com.continuent.tungsten.replicator.plugin.PluginContext)
+     */
+    public void shutdown(PluginContext context) throws ReplicatorException,
+            InterruptedException
+    {
+        // Stop the connector.
+        if (logger.isDebugEnabled())
+        {
+            logger.debug("Shutdown hook invoked; attempting to close connector");
+        }
+        shutdown = true;
+        release(context);
+    }
+
+    // Reconnect a failed connection.
+    private void reconnect() throws InterruptedException, ReplicatorException
+    {
+        synchronized (this)
+        {
+            if (conn != null)
+            {
+                conn.close();
+                conn = null;
+            }
+        }
+
+        // Reconnect after lost connection.
+        logger.info("Connection to remote thl lost; reconnecting");
+        pluginContext.getEventDispatcher().put(new OutOfSequenceNotification());
+        openConnector();
+    }
+
+    // Open up the connector here.
+    private void openConnector() throws ReplicatorException,
+            InterruptedException
+    {
+        // Connect to remote THL.
+        ConnectUriManager uriManager = new ConnectUriManager(uriList);
+        logger.info("Opening connection to master: " + uriManager.toString());
+        String currentUri = null;
+        long retryCount = 0;
+        for (;;)
+        {
+            try
+            {
+                // If we need to check serialization we must supply the seqno
+                // and epoch.
+                try
+                {
+                    currentUri = uriManager.next();
+                    conn = (Connector) PluginLoader
+                            .load(pluginContext
+                                    .getReplicatorProperties()
+                                    .getString(
+                                            ReplicatorConf.THL_PROTOCOL,
+                                            ReplicatorConf.THL_PROTOCOL_DEFAULT,
+                                            false));
+                    conn.setURI(currentUri);
+                    conn.setResetPeriod(resetPeriod);
+                    conn.setHeartbeatMillis(heartbeatMillis);
+                    conn.setLastEventId(this.lastEventId);
+                    if (this.lastEvent == null
+                            || this.checkSerialization == false)
+                    {
+                        conn.setLastSeqno(-1);
+                        conn.setLastEpochNumber(-1);
+                    }
+                    else
+                    {
+                        conn.setLastSeqno(lastEvent.getSeqno());
+                        conn.setLastEpochNumber(lastEvent.getEpochNumber());
+                    }
+                    conn.configure(pluginContext);
+                    conn.prepare(pluginContext);
+                }
+                catch (ReplicatorException e)
+                {
+                    throw new THLException("Error while initializing plug-in ",
+                            e);
+                }
+
+                conn.connect();
+                break;
+            }
+            catch (IOException e)
+            {
+                // Sleep for 1 second per retry; report every 10 retries.
+                synchronized (this)
+                {
+                    // Clearing the connection must be synchronized.
+                    // See concurrency note in class header comment.
+                    if (conn != null)
+                    {
+                        conn.close();
+                        conn = null;
+                    }
+                }
+                if ((retryCount % 10) == 0)
+                {
+                    logger.info("Waiting for master to become available: uri="
+                            + uriManager.toString() + " retries=" + retryCount);
+                }
+                retryCount++;
+                Thread.sleep(1000);
+            }
+        }
+
+        // Announce the happy event and reset retry count.
+        logger.info("Connected to master on uri=" + currentUri + " after "
+                + retryCount + " retries");
+        retryCount = 0;
+        pluginContext.getEventDispatcher().put(new InSequenceNotification());
+    }
+}
